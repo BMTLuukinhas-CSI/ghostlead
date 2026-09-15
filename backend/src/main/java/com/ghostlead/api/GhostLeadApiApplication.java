07210588aa6fe7c30406ee65088eea60cef08f69
Unstopable <unstopable@kali.Lucas>
2026-09-15 18:28:20 -0300

    chore: inicializa backend do GhostLead

---
+++ backend/src/main/java/com/ghostlead/api/GhostLeadApiApplication.java
@@ -0,0 +1,13 @@
+package com.ghostlead.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhostLeadApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhostLeadApiApplication.class, args);
+	}
+
+}
